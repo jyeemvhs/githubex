8f5f5c6c417f410e654245dd969a71809bad2100
jyeemvhs <jeffreyyee@vistausd.org>
2022-11-14 19:22:14 -0800

    Initial commit

---
+++ GitHubEx.java
@@ -0,0 +1,11 @@
+
+package githubex;
+
+public class GitHubEx {
+
+    public static void main(String[] args) {
+        Stuff ptr = new Stuff(7);
+        System.out.println(ptr.getValue());
+    }
+    
+}
---
+++ Stuff.java
@@ -0,0 +1,12 @@
+
+package githubex;
+
+public class Stuff {
+    private int value;
+    Stuff(int _value) {
+        value = _value;
+    }
+    public int getValue() {
+        return (value);
+    }
+}
