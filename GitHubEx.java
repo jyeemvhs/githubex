2c624f5e5e0dd3dee075f2123493374d4acb413e
jyeelsm <jeff_yee@live.com>
2022-11-14 20:02:52 -0800

    Update GitHubEx.java
    Changed 7 to 9

--- GitHubEx.java
+++ GitHubEx.java
@@ -4,7 +4,7 @@ package githubex;
 public class GitHubEx {
 
     public static void main(String[] args) {
-        Stuff ptr = new Stuff(7);
+        Stuff ptr = new Stuff(9);
         System.out.println(ptr.getValue());
     }
     
