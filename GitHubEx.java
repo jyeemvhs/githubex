16760597589ab35e796de8a4d06d7d0fa8d647e9
jyeelsm <jeff_yee@live.com>
2022-11-15 19:15:23 -0800

    Update GitHubEx.java
    Add comment

--- GitHubEx.java
+++ GitHubEx.java
@@ -5,7 +5,7 @@ public class GitHubEx {
 
     public static void main(String[] args) {
         Stuff ptr = new Stuff(5);
-        System.out.println(ptr.getValue());
+        System.out.println(ptr.getValue());   //Print the value
     }
     
 }
